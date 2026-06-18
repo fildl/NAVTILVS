df80efabde73248f12191508c42c0909b9232a65
filo <filippodiludovico@icloud.com>
2026-06-18 15:50:24 +0200

    [feat] edited cavity simulation using Classes

--- src/examples/cavity.py
+++ src/examples/cavity.py
@@ -1,46 +1,31 @@
-import numpy as np
-from ns_solver.solver import NS_solver
-from plots.plot import plot_stream
-
-# Parameters
-# Geometric parameters
-NX = 41
-NY = 41
-LX = 2.0
-LY = 1.0
-NT = 500
-DT = 0.001
+from ns_solver import Grid
+from ns_solver import CavitySimulation
+from plots import plot_stream
 
 # Physics parameters
 RHO = 1.0 # Density
 NU = 0.1  # Kinematic viscosity
 
-# Spacial steps
-dx = LX / (NX - 1)
-dy = LY / (NY - 1)
+# Define grid
+grid = Grid(lx = 1.0,
+             ly = 1.0,
+             nx = 41,
+             ny = 41)
 
-# Phisical fileds
-u = np.zeros((NY, NX))
-v = np.zeros((NY, NX))
-p = np.zeros((NY, NX))
+# Define simulation
+simulation = CavitySimulation(grid=grid,
+                              rho=RHO,
+                              nu=NU,
+                              dt=0.001)
 
-# Run simulaion
-print("Cavity Flow Simulation running")
-u, v, p = NS_solver(
-    u=u, v=v, p=p,
-    dx=dx, dy=dy,
-    nx=NX, ny=NY,
-    rho=RHO,
-    nu=NU,
-    nt=NT,
-    dt=DT
-)
+# Run simulation
+u, v, p = simulation.solve(nt=500)
 
 # Plot
 plot_stream(
     u=u, v=v, p=p,
-    lx=LX, ly=LY,
-    nx=NX, ny=NY,
+    lx=grid.lx, ly=grid.ly,
+    nx=grid.nx, ny=grid.ny,
     rho=RHO,
     nu=NU
 )
