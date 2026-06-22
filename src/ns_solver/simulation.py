e1bc9810d624290172c438104c5cacb6e310453b
filo <filippodiludovico@icloud.com>
2026-06-22 16:14:36 +0200

    [feat] implemented dynamic time-stepping in SimulationClass

--- src/ns_solver/simulation.py
+++ src/ns_solver/simulation.py
@@ -37,11 +37,50 @@ class SimulationClass:
             raise ValueError("Viscosity must be strictly positive.")
         if self.dt <= 0:
             raise ValueError("Time step (dt) must be strictly positive.")
+        
+        # Store the input time step for compute dynamic time stepping
+        self.dt_max = self.dt
 
         # Initialize velocity and pressure fields
         self.u = np.zeros((self.grid.ny, self.grid.nx))
         self.v = np.zeros((self.grid.ny, self.grid.nx))
         self.p = np.zeros((self.grid.ny, self.grid.nx))
+
+    def compute_dynamic_dt(self) -> float:
+        """
+        Compute the time step :math:`dt` based on CFL and viscous limits.
+
+        For the derivation and context, see :ref:`numerical_stability`.
+
+        Returns
+        -------
+        float
+            The dynamically computed stable time step size.
+        """
+
+        dx = self.grid.dx
+        dy = self.grid.dy
+
+        # 1. Convective Limit (CFL condition)
+        # Define maximum velocity in the domain
+        max_u = np.max(np.abs(self.u))
+        max_v = np.max(np.abs(self.v))
+
+        # Avoid division by zero if the fluid is completely at rest
+        if max_u + max_v == 0.0:
+            dt_cfl = float('inf')
+        else:
+            dt_cfl = 0.5 / (max_u / dx + max_v / dy)
+
+        # 2. Viscous Limit (Diffusive limit)
+        # C_visc è il fattore di sicurezza (tipicamente 0.9)
+        denom_visc = 2.0 * self.nu * (dx**2 + dy**2)
+        dt_visc = 0.9 * (dx**2 * dy**2) / denom_visc
+
+        # Define time step as the minimum of the two limits
+        stable_dt = min(dt_cfl, dt_visc)
+
+        return min(stable_dt, self.dt_max)
 
     def pressure_bc(self,
                     p : np.ndarray
@@ -84,8 +123,11 @@ class SimulationClass:
 
     def step(self):
         """
-        Solve NS equations for one time step.
+        Solve NS equations for one time step using dynamic time-stepping.
         """
+
+        # Compute dynamic time step
+        self.dt = self.compute_dynamic_dt()
 
         dx = self.grid.dx
         dy = self.grid.dy
