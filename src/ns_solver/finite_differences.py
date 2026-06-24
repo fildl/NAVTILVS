197a96877a65cd93581855911affbadbaeec05ec
filo <filippodiludovico@icloud.com>
2026-06-24 12:55:07 +0200

    [feat] added laplacian_2d function

--- src/ns_solver/finite_differences.py
+++ src/ns_solver/finite_differences.py
@@ -119,3 +119,39 @@ def centered_diff_y(f: np.ndarray,
         raise ValueError(f"Input array must be 2-dimensional. Got {f.ndim}D instead.")
     
     return (f[2:, 1:-1] - f[0:-2, 1:-1]) / (2 * dy)
+
+def laplacian_2d(f: np.ndarray,
+                 dx: float,
+                 dy: float
+                 ) -> np.ndarray:
+    """
+    Compute the Laplacian of a 2D array using second order central differences.
+
+    Parameters
+    ----------
+
+    f : np.ndarray
+        2D array for which to compute the Laplacian.
+    dx : float
+        Grid spacing in the x-direction.
+    dy : float
+        Grid spacing in the y-direction.
+
+    Returns
+    -------
+    np.ndarray
+        2D array with Laplacian applied.
+
+    Raises
+    ------
+    ValueError
+        If the input array is not 2-dimensional.
+    """
+
+    if f.ndim != 2:
+        raise ValueError(f"Input array must be 2-dimensional. Got {f.ndim}D instead.")
+    
+    d2f_dx2 = (f[1:-1, 2:] - 2 * f[1:-1, 1:-1] + f[1:-1, 0:-2]) / dx**2
+    d2f_dy2 = (f[2:, 1:-1] - 2 * f[1:-1, 1:-1] + f[0:-2, 1:-1]) / dy**2
+    
+    return d2f_dx2 + d2f_dy2
