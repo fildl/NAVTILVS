a36b915961f183e08d1b959c9acd95c6ef1b59a7
filo <filippodiludovico@icloud.com>
2026-06-27 10:46:18 +0200

    [feat] added CylinderSimulation to imports

--- src/ns_solver/__init__.py
+++ src/ns_solver/__init__.py
@@ -1,6 +1,6 @@
 # src/ns_solver/__init__.py
 from .grid import Grid
-from .simulation import SimulationClass, CavitySimulation
+from .simulation import SimulationClass, CavitySimulation, CylinderSimulation
 from .solver import build_up_b, pressure_poisson, update_velocity
 from .finite_differences import backward_diff_x, backward_diff_y, centered_diff_x, centered_diff_y, laplacian_2d
 
@@ -8,6 +8,7 @@ __all__ = [
     "Grid",
     "SimulationClass",
     "CavitySimulation",
+    "CylinderSimulation",
     "build_up_b",
     "pressure_poisson",
     "update_velocity",
--- src/plots/__init__.py
+++ src/plots/__init__.py
@@ -1,6 +1,8 @@
 # src/plots/__init__.py
-from .plot import plot_stream
+from .plot import plot_stream, plot_cylinder_flow
+
 
 __all__ = [
     "plot_stream",
+    "plot_cylinder_flow",
 ]
