0e8bdc3279035945c4dcec3d55b1d380bd91f366
filo <filippodiludovico@icloud.com>
2026-09-06 11:07:48 +0200

    [feat] added forward difference functions to finite differences import

--- src/ns_solver/__init__.py
+++ src/ns_solver/__init__.py
@@ -2,7 +2,12 @@
 from .grid import Grid
 from .simulation import SimulationClass, CavitySimulation, CylinderSimulation
 from .solver import build_up_b, pressure_poisson, update_velocity
-from .finite_differences import backward_diff_x, backward_diff_y, centered_diff_x, centered_diff_y, laplacian_2d
+from .finite_differences import (
+    backward_diff_x, backward_diff_y,
+    forward_diff_x, forward_diff_y,
+    centered_diff_x, centered_diff_y,
+    laplacian_2d
+)
 
 __all__ = [
     "Grid",
@@ -14,6 +19,8 @@ __all__ = [
     "update_velocity",
     "backward_diff_x",
     "backward_diff_y",
+    "forward_diff_x",
+    "forward_diff_y",
     "centered_diff_x",
     "centered_diff_y",
     "laplacian_2d",
