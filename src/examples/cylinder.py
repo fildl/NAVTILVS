de267af05796a4c547d25c77e2048f912dc09ae7
filo <filippodiludovico@icloud.com>
2026-09-07 11:21:47 +0200

    [parameters] updatedcylinder simulation parameters

--- src/examples/cylinder.py
+++ src/examples/cylinder.py
@@ -4,42 +4,52 @@ from plots import plot_cylinder_flow
 
 # Physics parameters
 RHO = 1.0 # Density
-NU = 0.005  # Kinematic viscosity
+NU = 0.001  # Kinematic viscosity
 
 # Define grid
 grid = Grid(lx = 2.0,
             ly = 0.5,
-            nx = 128,
-            ny = 32)
+            nx = 256,
+            ny = 64)
 
 # Define simulation
 simulation = CylinderSimulation(grid=grid,
                                 rho=RHO,
                                 nu=NU,
                                 dt=0.005,
-                                cylinder_center=(0.4, 0.25),
+                                cylinder_center=(0.4, 0.225),
                                 cylinder_radius=0.05,
                                 u_inlet=1.0)
 
 # Run simulation
 print("Running solver...")
-u, v, p = simulation.solve(nt=None, t_end=5.0)
+print(f"Reynolds number: {simulation.reynolds_number:.2f}")
+u, v, p = simulation.solve(nt=None, t_end=3.5)
 print(f"Simulation completed! Simulated time: {simulation.t:.3f} s")
 
 # Plot Vorticity
 plot_cylinder_flow(u=u, v=v, p=p,
                    grid=grid,
                    obstacle_mask=simulation.obstacle_mask,
-                   mode='vorticity')
+                   mode='vorticity',
+                   t=simulation.t,
+                   reynolds=simulation.reynolds_number,
+                   save_path='imgs/cylinder_vorticity.png')
 
 # Plot Velocity
 plot_cylinder_flow(u=u, v=v, p=p,
                    grid=grid,
                    obstacle_mask=simulation.obstacle_mask,
-                   mode='velocity')
+                   mode='velocity',
+                   t=simulation.t,
+                   reynolds=simulation.reynolds_number,
+                   save_path='imgs/cylinder_velocity.png')
 
 # Plot Pressure
 plot_cylinder_flow(u=u, v=v, p=p,
                    grid=grid,
                    obstacle_mask=simulation.obstacle_mask,
-                   mode='pressure')
+                   mode='pressure',
+                   t=simulation.t,
+                   reynolds=simulation.reynolds_number,
+                   save_path='imgs/cylinder_pressure.png')
