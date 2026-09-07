2d5e8d6772825c109a7e8048a26c7174d5c77411
filo <filippodiludovico@icloud.com>
2026-09-07 11:16:55 +0200

    [fix] updated imports to replace plot_stream with plot_cavity_flow

--- src/plots/__init__.py
+++ src/plots/__init__.py
@@ -1,8 +1,8 @@
 # src/plots/__init__.py
-from .plot import plot_stream, plot_cylinder_flow
+from .plot import plot_cavity_flow, plot_cylinder_flow
 
 
 __all__ = [
-    "plot_stream",
+    "plot_cavity_flow",
     "plot_cylinder_flow",
 ]
