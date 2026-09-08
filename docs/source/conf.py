be092b115df862fd5f8246822b135f63ddb184a2
filo <filippodiludovico@icloud.com>
2026-09-08 11:55:57 +0200

    [docs] updated Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,19 +3,21 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+from ns_solver import __version__ as release
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'NAVTILVS'
 copyright = '2026, Filippo Di Ludovico'
 author = 'Filippo Di Ludovico'
-release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../src'))
 
 extensions = [
     'sphinx.ext.autodoc',
